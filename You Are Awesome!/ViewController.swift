95039cd59d2e59624ae1cca94203d631f84ea40c
Brian Wang <BrianWang@brians-mbp.bc.edu>
2018-01-29 16:01:19 -0500

    Ji Woo Pak

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,18 +7,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var awesomeImage: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    var soundNumber = -1
+    var numberOfImages = 2
+    var numberOfSounds = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You Are Fantastic!",
+                        "You Are Great!",
+                        "You Are Amazing!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You Brighten My Day!",
+                        "You Are Da Bomb!",
+                        "I can't wait to use your app!"]
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        }while index == newIndex
+        index = newIndex
+        messageLabel.text = messages[newIndex]
+        
+        awesomeImage.isHidden = false
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(numberOfImages)))
+            
+        } while imageNumber == newIndex
+        
+        imageNumber = newIndex
+        awesomeImage.image = UIImage(named: "image\(imageNumber)")
+        
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(numberOfSounds)))
+        } while soundNumber == newIndex
+        
+        soundNumber = newIndex
+        
+        var soundName = "sound\(soundNumber)"
+        
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            } catch {
+                print("ERROR: \(soundName)")
+            }
+        } else {
+            print("ERROR: file \(soundName) didn't load")
+        }
     }
+    
+    
+    
 
 
 }
